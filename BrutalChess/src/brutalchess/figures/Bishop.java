34b99c80627c34806a57f313ce3ee29988eceea9
Barush <bara.skrivankova@gmail.com>
2014-06-11 13:08:08 +0200

    add Bishop, King and Rook with their own canMove method

---
+++ BrutalChess/src/brutalchess/figures/Bishop.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brutalchess.figures;
+
+import static brutalchess.Const.*;
+import brutalchess.basis.Figure;
+import brutalchess.basis.Position;
+import static java.lang.Math.abs;
+
+/**
+ *
+ * @author xskriv01
+ */
+public class Bishop extends Figure{
+ 
+    public Bishop(Position p, int color){
+       super(p, color);
+    }
+
+    @Override
+    public boolean canMove(Position p) {
+        if(abs(this.getPosition().getCol() - p.getCol()) == abs(this.getPosition().getRow() - p.getRow())){
+            return true;
+        }
+        return false;
+    }
+}
+
---
+++ BrutalChess/src/brutalchess/figures/King.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brutalchess.figures;
+
+import static brutalchess.Const.*;
+import brutalchess.basis.Figure;
+import brutalchess.basis.Position;
+import static java.lang.Math.abs;
+
+/**
+ *
+ * @author xskriv01
+ */
+public class King extends Figure{
+ 
+    public King(Position p, int color){
+       super(p, color);
+    }
+
+    @Override
+    public boolean canMove(Position p) {
+       if((abs(this.getPosition().getCol() - p.getCol()) <= 1) && (abs(this.getPosition().getRow() - p.getRow()) <= 1)){
+           return true;
+       }
+       return false;
+    }
+}
+
